b2112a6d270ca3358b86b2310d8bed64524cdb50
Moosa <computerknowledge817@gmail.com>
2023-03-11 03:26:04 +0500

    Revert "waifu done :)"
    This reverts commit bc7207680eacff6a0c43011edd19068967645bcc.
    
    	modified:   mycogs/chat.py

--- mycogs/chat.py
+++ mycogs/chat.py
@@ -2,64 +2,18 @@ from discord.ext import commands
 import config
 from pymongo import MongoClient
 from discord import Webhook
+import asyncio
 import aiohttp
-import json
-
-BOT_NAME = 'Waifu' 
-SITUATION = 'Waifu talks to you'
-X_RAPIDAPI_KEY = "4b1d765b96msh163b83ada64ba48p150ed4jsn7b12f2017f94" 
-TRANSLATE_FROM = 'en' 
-TRANSLATE_TO = 'en' 
-
-
 
 client = MongoClient(config.mongo_str)
 db = client.get_database('cul_bot')
 records = db.server_info
-
-
-async def waifu_ai_query(query, user_id, user_name):
-    url = "https://waifu.p.rapidapi.com/path"
-
-    querystring = {
-        "user_id": user_id,
-        "message": query,
-        "from_name": user_name, 
-        "to_name" : BOT_NAME,
-        "situation": SITUATION,
-        "translate_from": TRANSLATE_FROM,
-        "translate_to": TRANSLATE_TO
-    }
-    
-    my_obj = {
-        "key1": "value",
-        "key2": "value"
-    }
-    
-    payload = json.dumps(my_obj)
-
-    headers = {
-    'content-type': "application/json",
-    'x-rapidapi-host': "waifu.p.rapidapi.com",
-    'x-rapidapi-key': X_RAPIDAPI_KEY
-    }
-    
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, data=payload, headers=headers, params=querystring) as response:
-            reply = await response.text(
-                encoding='utf-8'
-            )
-
-    return reply
-
-          
+                
 
 class chat(commands.Cog):
 
     def __init__(self,client):
         self.client = client
-
-    
 
     @commands.Cog.listener()
     async def on_message(self,message):
@@ -91,10 +45,8 @@ class chat(commands.Cog):
 
             async with aiohttp.ClientSession() as session:
                 webhook = Webhook.from_url(WEBHOOK_URL,session=session) # Initializing webhook
-                query = message.content
-                response = await waifu_ai_query(query, message.author.id, message.author.name)
 
-                await webhook.send(content=response, username='Waifu', avatar_url=PROFILE_PICTURE_URL)
+                await webhook.send(content=MESSAGE_CONTENT, username='Waifu', avatar_url=PROFILE_PICTURE_URL)
         else:
             pass
             return
